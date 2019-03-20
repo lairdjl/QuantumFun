e0af00be50e8fdab2050195be6ec72667a6e0fcb
Jacob Laird <jake@Jacobs-iMac-Pro.local>
2019-03-19 22:19:50 -0400

    cleaned up the code to run various tests

--- Driver.cs
+++ Driver.cs
@@ -7,23 +7,79 @@ namespace Bell
 {
     class Driver
     {
+        enum TEST_TO_RUN {
+            BELLTEST_SINGLE,
+            BELLTEST_DOUBLE
+        } 
         static void Main(string[] args)
         {
-            using (var qsim = new QuantumSimulator())
-            {
-                // Try initial values
-                Result[] initials = new Result[] { Result.Zero, Result.One };
-                foreach (Result initial in initials)
-                {
-                    var res = BellTest.Run(qsim, 1000, initial).Result;
-                    var (numZeros, numOnes) = res;
-                    System.Console.WriteLine(
-                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
-                }
+                TEST_TO_RUN testToRun = TEST_TO_RUN.BELLTEST_DOUBLE;
+
+                switch(testToRun){
+                    case TEST_TO_RUN.BELLTEST_SINGLE:
+                        _bellTestSingleQubit();
+                        break;
+                    case TEST_TO_RUN.BELLTEST_DOUBLE:
+                        _bellTestDoubleQubit();
+                        break;
+                    default:
+
+                    break;
+
             }
 
             System.Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+        }
+
+        static void _bellTestSingleQubit(){
+             using (var qsim = new QuantumSimulator()){
+
+                var estimator = new ResourcesEstimator();
+                BellTestSingleQubit.Run(estimator, 1000, Result.Zero).Wait();
+
+                System.Console.WriteLine(estimator.ToTSV());
+            
+                System.Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+
+                // Try initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                System.Console.WriteLine("** BELL TEST SINGLE QUBIT **");
+                    foreach (Result initial in initials)
+                {
+                    var res = BellTestSingleQubit.Run(qsim, 1000, initial).Result;
+                    var (numZeros, numOnes) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                }
+        }
+        }
+
+                static void _bellTestDoubleQubit(){
+             using (var qsim = new QuantumSimulator()){
+
+                var estimator = new ResourcesEstimator();
+                BellTestTwoQubits.Run(estimator, 1000, Result.Zero).Wait();
+
+                System.Console.WriteLine(estimator.ToTSV());
+            
+                System.Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+
+                // Try initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+
+                System.Console.WriteLine("** BELL TEST TWO QUBITS **");
+
+                foreach (Result initial in initials)
+                {
+                    var res = BellTestTwoQubits.Run(qsim, 1000, initial).Result;
+                    var (numZeros, numOnes, agree) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree, -4}");
+                }
+        }
         }
     }
 }
--- Operations.qs
+++ Operations.qs
@@ -12,7 +12,7 @@
         }
     }
 
-     operation BellTest (count : Int, initial: Result) : (Int, Int)
+     operation BellTestSingleQubit (count : Int, initial: Result) : (Int, Int)
     {
         mutable numOnes = 0;
         using (qubit = Qubit())
@@ -41,5 +41,43 @@
 
         // Return number of times we saw a |0> and number of times we saw a |1>
         return (count-numOnes, numOnes);
+    }
+
+    operation BellTestTwoQubits(count : Int, initial: Result): (Int, Int, Int)
+    {
+        mutable numOnes = 0;
+        mutable agree = 0;
+        using (qubits = Qubit[2])
+        {
+            for(test in 1..count)
+            {
+                Set (initial, qubits[0]);
+                Set (Zero, qubits[1]);
+
+                H(qubits[0]);
+
+                //CNOT Entangles the qubits, so that whatever happens to one of them, happens to the other.
+                CNOT(qubits[0], qubits[1]);
+                let res = M (qubits[0]);
+
+                //see how the second qubit reacts to the first being measured
+                if(M (qubits[1]) == res)
+                {
+                    set agree = agree + 1;
+                }
+
+                //count the number of ones we saw:
+                if(res == One){
+                    set numOnes = numOnes + 1;
+                }
+            }
+            
+            Set(Zero, qubits[0]);
+            Set(Zero, qubits[1]);
+        }
+
+
+        // Return number of times we saw a |0> and number of times we saw a |1>
+        return (count-numOnes, numOnes, agree);
     }
 }
